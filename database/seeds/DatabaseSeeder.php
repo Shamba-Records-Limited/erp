c36ee9a012557b3966adbb7a57736a57bc8697ef
de-wan <stephen.wandie@churpy.co>
2024-06-14 08:14:04 +0300

    feat(seeder): updated database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-   //   $this->call(RolesNPermissionsSeeder::class);
-   //    $this->call(CooperativeSeeder::class);
+        $this->call(RolesNPermissionsSeeder::class);
+        $this->call(CooperativeSeeder::class);
         $this->call(UserSeeder::class);
-
+        $this->call(CountiesSeeder::class);
+        $this->call(SubCountiesSeeder::class);
     }
 }
