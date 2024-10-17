2988e0db9348f1ddf9331b1372f4d65cc75d8aaa
Priscilla-22 <priscillamw123@gmail.com>
2024-10-17 21:49:21 +0300

    feat: data

--- public/argon/js/argon.js
+++ public/argon/js/argon.js
@@ -943,80 +943,70 @@ var Charts = (function () {
 // Orders chart
 //
 
-var OrdersChart = (function () {
-  //
-  // Variables
-  //
+// var OrdersChart = (function () {
 
-  var $chart = $('#GradeDistributionBarChart');
-  var $GradeDistributionBarChart = $('[name="ordersSelect"]');
 
-  //
-  // Methods
-  //
+//   var $chart = $('#GradeDistributionBarChart');
+//   var $GradeDistributionBarChart = $('[name="ordersSelect"]');
 
-  // Init chart
-  function initChart($chart) {
-    // Create chart
-    var GradeDistributionBarChart = new Chart($chart, {
-      type: 'bar',
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                callback: function (value) {
-                  if (!(value % 10)) {
-                    //return '$' + value + 'k'
-                    return value;
-                  }
-                },
-              },
-            },
-          ],
-        },
-        tooltips: {
-          callbacks: {
-            label: function (item, data) {
-              var label = data.datasets[item.datasetIndex].label || '';
-              var yLabel = item.yLabel;
-              var content = '';
 
-              if (data.datasets.length > 1) {
-                content +=
-                  '<span class="popover-body-label mr-auto">' +
-                  label +
-                  '</span>';
-              }
+//   function initChart($chart) {
+//     var GradeDistributionBarChart = new Chart($chart, {
+//       type: 'bar',
+//       options: {
+//         scales: {
+//           yAxes: [
+//             {
+//               ticks: {
+//                 callback: function (value) {
+//                   if (!(value % 10)) {
+//                     return value;
+//                   }
+//                 },
+//               },
+//             },
+//           ],
+//         },
+//         tooltips: {
+//           callbacks: {
+//             label: function (item, data) {
+//               var label = data.datasets[item.datasetIndex].label || '';
+//               var yLabel = item.yLabel;
+//               var content = '';
 
-              content +=
-                '<span class="popover-body-value">' + yLabel + '</span>';
+//               if (data.datasets.length > 1) {
+//                 content +=
+//                   '<span class="popover-body-label mr-auto">' +
+//                   label +
+//                   '</span>';
+//               }
 
-              return content;
-            },
-          },
-        },
-      },
-      data: {
-        labels: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        datasets: [
-          {
-            label: 'Sales',
-            data: [25, 20, 30, 22, 17, 29],
-          },
-        ],
-      },
-    });
+//               content +=
+//                 '<span class="popover-body-value">' + yLabel + '</span>';
 
-    // Save to jQuery object
-    $chart.data('chart', GradeDistributionBarChart);
-  }
+//               return content;
+//             },
+//           },
+//         },
+//       },
+//       data: {
+//         labels: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+//         datasets: [
+//           {
+//             label: 'Sales',
+//             data: [25, 20, 30, 22, 17, 29],
+//           },
+//         ],
+//       },
+//     });
 
-  // Init chart
-  if ($chart.length) {
-    initChart($chart);
-  }
-})();
+//     $chart.data('chart', GradeDistributionBarChart);
+//   }
+
+//   if ($chart.length) {
+//     initChart($chart);
+//   }
+// })();
 
 //
 // Charts
@@ -1028,74 +1018,71 @@ var OrdersChart = (function () {
 // Sales chart
 //
 
-var SalesChart = (function () {
-  // Variables
+// var SalesChart = (function () {
 
-  var $chart = $('#CollectionsBarChart');
+//   var $chart = $('#CollectionsBarChart');
 
-  // Methods
 
-  function init($chart) {
-    var CollectionsBarChart = new Chart($chart, {
-      type: 'bar',
-      options: {
-        scales: {
-          yAxes: [
-            {
-              gridLines: {
-                color: Charts.colors.gray[900],
-                zeroLineColor: Charts.colors.gray[900],
-              },
-              ticks: {
-                callback: function (value) {
-                  if (!(value % 10)) {
-                    return '$' + value + 'k';
-                  }
-                },
-              },
-            },
-          ],
-        },
-        tooltips: {
-          callbacks: {
-            label: function (item, data) {
-              var label = data.datasets[item.datasetIndex].label || '';
-              var yLabel = item.yLabel;
-              var content = '';
+//   function init($chart) {
+//     var CollectionsBarChart = new Chart($chart, {
+//       type: 'line',
+//       options: {
+//         scales: {
+//           yAxes: [
+//             {
+//               gridLines: {
+//                 color: Charts.colors.gray[900],
+//                 zeroLineColor: Charts.colors.gray[900],
+//               },
+//               ticks: {
+//                 callback: function (value) {
+//                   if (!(value % 10)) {
+//                     return '$' + value + 'k';
+//                   }
+//                 },
+//               },
+//             },
+//           ],
+//         },
+//         tooltips: {
+//           callbacks: {
+//             label: function (item, data) {
+//               var label = data.datasets[item.datasetIndex].label || '';
+//               var yLabel = item.yLabel;
+//               var content = '';
 
-              if (data.datasets.length > 1) {
-                content +=
-                  '<span class="popover-body-label mr-auto">' +
-                  label +
-                  '</span>';
-              }
+//               if (data.datasets.length > 1) {
+//                 content +=
+//                   '<span class="popover-body-label mr-auto">' +
+//                   label +
+//                   '</span>';
+//               }
 
-              content +=
-                '<span class="popover-body-value">$' + yLabel + 'k</span>';
-              return content;
-            },
-          },
-        },
-      },
-      data: {
-        labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        datasets: [
-          {
-            label: 'Weight in Kgs',
-            data: [0, 20, 10, 30, 15, 40, 20, 60, 60],
-          },
-        ],
-      },
-    });
+//               content +=
+//                 '<span class="popover-body-value">$' + yLabel + 'k</span>';
+//               return content;
+//             },
+//           },
+//         },
+//       },
+//       data: {
+//         labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+//         datasets: [
+//           {
+//             label: 'Weight in Kgs',
+//             data: [0, 20, 10, 30, 15, 40, 20, 60, 60],
+//           },
+//         ],
+//       },
+//     });
 
-    // Save to jQuery object
 
-    $chart.data('chart', CollectionsBarChart);
-  }
+//     $chart.data('chart', CollectionsBarChart);
+//   }
 
-  // Events
 
-  if ($chart.length) {
-    init($chart);
-  }
-})();
+//   if ($chart.length) {
+//     init($chart);
+//   }
+// }
+
--- resources/views/pages/cooperative-admin/dashboard.blade.php
+++ resources/views/pages/cooperative-admin/dashboard.blade.php
@@ -58,34 +58,37 @@ $total_gender_distribution = $data["gender"]->female + $data["gender"]->male + $
 @endphp
 @include('layouts.headers.cards')
 
-<!-- <div class="d-flex justify-content-between w-100">
+<div class="container-fluid mt--7">
 
-    <div>Dashboard</div>
-    <div class="d-flex align-items-start">
-        <form class="d-flex">
-            <div class="form-group">
-                <select name="date_range" placeholder="Select Date Range" class="form-control select2bs4"
-                    onchange="this.form.submit()" id="dateRange">
-                    <option value="week" @if($date_range=="week" ) selected @endif)>This Week</option>
-                    <option value="month" @if($date_range=="month" ) selected @endif>This Month</option>
-                    <option value="year" @if($date_range=="year" ) selected @endif>This Year</option>
-                    <option value="custom" @if($date_range=="custom" ) selected @endif>Custom</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <input type="date" class="form-control" name="from_date" value="{{$from_date}}"
-                    onchange="this.form.submit()" id="fromDate" />
-            </div>
-            <div class="form-group">
-                <input type="date" class="form-control" name="to_date" value="{{$to_date}}"
-                    onchange="this.form.submit()" id="toDate" />
-            </div>
-        </form>
-        <button class="btn btn-warning mt-1 ml-2" href="{{route('cooperative-admin.dashboard.export')}}"
-            onclick="exportChart()">Export</button>
+    <div class="d-flex justify-content-between w-100">
+
+        <div>Dashboard</div>
+        <div class="d-flex align-items-start">
+            <form class="d-flex">
+                <div class="form-group">
+                    <select name="date_range" placeholder="Select Date Range" class="form-control select2bs4"
+                        onchange="this.form.submit()" id="dateRange">
+                        <option value="week" @if($date_range=="week" ) selected @endif)>This Week</option>
+                        <option value="month" @if($date_range=="month" ) selected @endif>This Month</option>
+                        <option value="year" @if($date_range=="year" ) selected @endif>This Year</option>
+                        <option value="custom" @if($date_range=="custom" ) selected @endif>Custom</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <input type="date" class="form-control" name="from_date" value="{{$from_date}}"
+                        onchange="this.form.submit()" id="fromDate" />
+                </div>
+                <div class="form-group">
+                    <input type="date" class="form-control" name="to_date" value="{{$to_date}}"
+                        onchange="this.form.submit()" id="toDate" />
+                </div>
+            </form>
+            <button class="btn btn-warning mt-1 ml-2" href="{{route('cooperative-admin.dashboard.export')}}"
+                onclick="exportChart()">Export</button>
+        </div>
+
     </div>
 
-</div> -->
 
 
 
@@ -94,98 +97,118 @@ $total_gender_distribution = $data["gender"]->female + $data["gender"]->male + $
 
 
 
+    <div class="row">
 
-
-<div class="col-xl-8 mb-5 mb-xl-0">
-    <div class="card bg-gradient-default shadow">
-        <div class="card-header bg-transparent">
-            <div class="row align-items-center">
-                <div class="col">
-                    <h6 class="text-uppercase text-light ls-1 mb-1">Overview</h6>
-                    <h2 class="text-white mb-0">Collections Weight (KGs)</h2>
+        <div class="col-xl-8 mb-5 mb-xl-0">
+            <div class="card bg-gradient-default shadow">
+                <div class="card-header bg-transparent">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h6 class="text-uppercase text-light ls-1 mb-1">Overview</h6>
+                            <h2 class="text-white mb-0">Collections Weight (KGs)</h2>
+                        </div>
+                        <div class="col">
+                            <ul class="nav nav-pills justify-content-end">
+                                <!-- All Option -->
+                                <li class="nav-item mr-2 mr-md-0" data-toggle="chart" data-target="#CollectionsBarChart"
+                                    <a href="#" class="nav-link py-2 px-3 active" data-toggle="tab">
+                                    <span class="d-none d-md-block">All</span>
+                                    <span class="d-md-none">All</span>
+                                    </a>
+                                </li>
+                                <!-- Month Option -->
+                                <li class="nav-item mr-2 mr-md-0" data-toggle="chart" data-target="#CollectionsBarChart"
+                                    <a href="#" class="nav-link py-2 px-3" data-toggle="tab">
+                                    <span class="d-none d-md-block">Month</span>
+                                    <span class="d-md-none">M</span>
+                                    </a>
+                                </li>
+                                <!-- Week Option -->
+                                <li class="nav-item" data-toggle="chart" data-target="#CollectionsBarChart"
+                                    data-prefix="KG" <a href="#" class="nav-link py-2 px-3" data-toggle="tab">
+                                    <span class="d-none d-md-block">Week</span>
+                                    <span class="d-md-none">W</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div class="col">
-                    <ul class="nav nav-pills justify-content-end">
-                        <!-- All Option -->
-                        <li class="nav-item mr-2 mr-md-0" data-toggle="chart" data-target="#CollectionsBarChart" <a
-                            href="#" class="nav-link py-2 px-3 active" data-toggle="tab">
-                            <span class="d-none d-md-block">All</span>
-                            <span class="d-md-none">All</span>
-                            </a>
-                        </li>
-                        <!-- Month Option -->
-                        <li class="nav-item mr-2 mr-md-0" data-toggle="chart" data-target="#CollectionsBarChart" <a
-                            href="#" class="nav-link py-2 px-3" data-toggle="tab">
-                            <span class="d-none d-md-block">Month</span>
-                            <span class="d-md-none">M</span>
-                            </a>
-                        </li>
-                        <!-- Week Option -->
-                        <li class="nav-item" data-toggle="chart" data-target="#CollectionsBarChart" data-prefix="KG" <a
-                            href="#" class="nav-link py-2 px-3" data-toggle="tab">
-                            <span class="d-none d-md-block">Week</span>
-                            <span class="d-md-none">W</span>
-                            </a>
-                        </li>
-                    </ul>
+                <div class="card-body">
+                    <!-- Chart -->
+                    <div class="chart">
+                        <!-- Chart wrapper -->
+                        <canvas id="CollectionsBarChart" class="chart-canvas "></canvas>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <div class="col-xl-4">
+            <div class="card shadow">
+                <div class="card-header bg-transparent">
+                    <div class="row align-items-center">
+                        <div class="col">
+                            <h6 class="text-uppercase text-muted ls-1 mb-1">Grade Distribution KGs</h6>
+                            <h2 class="mb-0">Grade Distribution</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <!-- Chart -->
+                    <div class="chart">
+                        <canvas id="GradeDistributionBarChart" class="chart-canvas"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class=" span-4 card" style="overflow-y: scroll; height:350px;">
         <div class="card-body">
-            <!-- Chart -->
-            <div class="chart">
-                <!-- Chart wrapper -->
-                <canvas id="CollectionsBarChart" class="chart-canvas "></canvas>
+            <div class="card-title">Gender Distribution</div>
+            <div class="row">
+                <div class="col-12 d-flex align-items-center">
+                    <canvas id="FarmersGenderDoughnutChart" class="mb-4 mb-md-0" style="height: 200px;"></canvas>
+                </div>
+            </div>
+            <div class="grid grid-cols-2">
+                <div class="p-4">
+                    <div>Male</div>
+                    <h3 id="maleCount">{{$data["gender"]->male}}</h3>
+                </div>
+                <div class="p-4">
+                    <div>Female</div>
+                    <h3 id="femaleCount">{{$data["gender"]->female}}</h3>
+                </div>
             </div>
         </div>
     </div>
-</div>
+    <div class="row mt-5">
 
-
-<div class=" span-4 card" style="overflow-y: scroll; height:350px;">
-    <div class="card-body">
-        <div class="card-title">Gender Distribution</div>
-        <div class="row">
-            <div class="col-12 d-flex align-items-center">
-                <canvas id="FarmersGenderDoughnutChart" class="mb-4 mb-md-0" style="height: 200px;"></canvas>
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title">
+                    Collections Weight (KGs) By Gender
+                </div>
+                <div>
+                    <canvas id="CollectionsGenderBarChart" class="mb-4 mb-md-0" height="250"></canvas>
+                </div>
             </div>
         </div>
-        <div class="grid grid-cols-2">
-            <div class="p-4">
-                <div>Male</div>
-                <h3 id="maleCount">{{$data["gender"]->male}}</h3>
-            </div>
-            <div class="p-4">
-                <div>Female</div>
-                <h3 id="femaleCount">{{$data["gender"]->female}}</h3>
-            </div>
-        </div>
-    </div>
-</div>
 
-<div class="card">
-    <div class="card-body">
-        <div class="card-title">
-            Collections Weight (KGs) By Gender
-        </div>
-        <div>
-            <canvas id="CollectionsGenderBarChart" class="mb-4 mb-md-0" height="250"></canvas>
-        </div>
-    </div>
-</div>
-
-<div class="card span-6" style="overflow-y: scroll; height:350px;">
-    <div class="card-body">
-        <div class="card-title">Collection By Wet Mills (KGs)</div>
-        <div class="row">
-            <div class="col-12 d-flex align-items-center">
-                <canvas id="WetMillCollectionsBarChart" class="mb-4 mb-md-0" style="height: 200px;"></canvas>
+        <div class="card span-6" style="overflow-y: scroll; height:350px;">
+            <div class="card-body">
+                <div class="card-title">Collection By Wet Mills (KGs)</div>
+                <div class="row">
+                    <div class="col-12 d-flex align-items-center">
+                        <canvas id="WetMillCollectionsBarChart" class="mb-4 mb-md-0" style="height: 200px;"></canvas>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
 
-<!-- <div class="card span-6" style="overflow-y: scroll; height:350px;">
+        <!-- <div class="card span-6" style="overflow-y: scroll; height:350px;">
     <div class="card-body">
         <div class="card-title">Grade Distribution KGs</div>
         <div class="row">
@@ -195,22 +218,7 @@ $total_gender_distribution = $data["gender"]->female + $data["gender"]->male + $
         </div>
     </div>
 </div> -->
-<div class="col-xl-4">
-    <div class="card shadow">
-        <div class="card-header bg-transparent">
-            <div class="row align-items-center">
-                <div class="col">
-                    <h6 class="text-uppercase text-muted ls-1 mb-1">Grade Distribution KGs</h6>
-                    <h2 class="mb-0">Grade Distribution</h2>
-                </div>
-            </div>
-        </div>
-        <div class="card-body">
-            <!-- Chart -->
-            <div class="chart">
-                <canvas id="GradeDistributionBarChart" class="chart-canvas"></canvas>
-            </div>
-        </div>
+
     </div>
 </div>
 </div>
