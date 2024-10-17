549f81ce5124c6c7d51a295aa2f185b7bc9b2221
de-wan <stephen.wandie@churpy.co>
2024-10-17 11:47:35 +0300

    fix(adminrole migration): don't try to create user if her/she exists

--- database/migrations/2024_10_14_111352_assign_adminrole.php
+++ database/migrations/2024_10_14_111352_assign_adminrole.php
@@ -15,45 +15,41 @@ class AssignAdminrole extends Migration
      */
     public function up()
     {
-        //
-        Artisan::call( 'db:seed',['--class'=>'UserSeeder',
-                                   '--force'=>true
-                      
-                      ]);
+        // check if user exists
+        $user = User::where('username', 'admin')->first();
+        if (!$user) {
+            // seed if user does not exist
+            Artisan::call('db:seed', [
+                '--class' => 'UserSeeder',
+                '--force' => true
 
-
-
-// Start the database transaction
-DB::transaction(function () {
-    // Find the user with username 'admin'
-    $user = User::where('username', 'admin')->first();
-    
-    if ($user) {
-        // Check if the 'admin' role exists
-        $role = Role::where('name', 'admin')->first();
-        
-        if ($role) {
-            // Assign the 'admin' role to the user
-            $user->assignRole('admin');
-        } else {
-            // Create the 'admin' role if it doesn't exist and assign it
-            $role = Role::create(['name' => 'admin']);
-            $user->assignRole($role);
+            ]);
         }
-    } else {
-        // Optional: Handle case where no user is found with the 'admin' username
-        throw new \Exception("User with username 'admin' not found.");
-    }
-});
 
 
 
+        // Start the database transaction
+        DB::transaction(function () {
+            // Find the user with username 'admin'
+            $user = User::where('username', 'admin')->first();
 
+            if ($user) {
+                // Check if the 'admin' role exists
+                $role = Role::where('name', 'admin')->first();
 
-
-
-
-
+                if ($role) {
+                    // Assign the 'admin' role to the user
+                    $user->assignRole('admin');
+                } else {
+                    // Create the 'admin' role if it doesn't exist and assign it
+                    $role = Role::create(['name' => 'admin']);
+                    $user->assignRole($role);
+                }
+            } else {
+                // Optional: Handle case where no user is found with the 'admin' username
+                throw new \Exception("User with username 'admin' not found.");
+            }
+        });
     }
 
     /**
