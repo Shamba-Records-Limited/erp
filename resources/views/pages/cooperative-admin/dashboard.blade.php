eaf6a2efbcaa362d13b04ab518d02cb2af46ae75
Priscilla-22 <priscillamw123@gmail.com>
2024-10-17 20:25:55 +0300

    changes

--- resources/views/pages/cooperative-admin/dashboard.blade.php
+++ resources/views/pages/cooperative-admin/dashboard.blade.php
@@ -52,10 +52,14 @@
 @endpush
 
 @section('content')
+
 @php
 $total_gender_distribution = $data["gender"]->female + $data["gender"]->male + $data["gender"]->other;
 @endphp
-<div class="d-flex justify-content-between w-100">
+@include('layouts.headers.cards')
+
+<!-- <div class="d-flex justify-content-between w-100">
+
     <div>Dashboard</div>
     <div class="d-flex align-items-start">
         <form class="d-flex">
@@ -81,75 +85,15 @@ $total_gender_distribution = $data["gender"]->female + $data["gender"]->male + $
             onclick="exportChart()">Export</button>
     </div>
 
-</div>
+</div> -->
 
 
 
-<div class="col-xl-3 col-lg-6">
-    <div class="card card-stats mb-4 mb-xl-0">
-        <div class="card-body">
-            <div class="row">
-                <div class="col">
-                    <h5 class="card-title text-uppercase text-muted mb-0">Collection Total Weight</h5>
-                    <span class="h2 font-weight-bold mb-0">{{$data["total_collection_weight"] ?? "0"}} KG</span>
-                </div>
-                <div class="col-auto">
-                    <div class="icon icon-shape bg-yellow text-white rounded-circle shadow">
-                        <i class="fas fa-users"></i>
-                    </div>
-                </div>
-            </div>
-            <p class="mt-3 mb-0 text-muted text-sm">
-                <span class="text-warning mr-2"><i class="fas fa-arrow-down"></i> 1.10%</span>
-                <span class="text-nowrap">Since yesterday</span>
-            </p>
-        </div>
-    </div>
-</div>
 
 
-<div class="col-xl-3 col-lg-6">
-    <div class="card card-stats mb-4 mb-xl-0">
-        <div class="card-body">
-            <div class="row">
-                <div class="col">
-                    <h5 class="card-title text-uppercase text-muted mb-0">Farmer Count</h5>
-                    <span class="h2 font-weight-bold mb-0">{{$data["farmer_count"]}}</span>
-                </div>
-                <div class="col-auto">
-                    <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
-                        <i class="fas fa-chart-bar"></i>
-                    </div>
-                </div>
-            </div>
-            <p class="mt-3 mb-0 text-muted text-sm">
-                <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                <span class="text-nowrap">Since last month</span>
-            </p>
-        </div>
-    </div>
-</div>
-<div class="col-xl-3 col-lg-6">
-    <div class="card card-stats mb-4 mb-xl-0">
-        <div class="card-body">
-            <div class="row">
-                <div class="col">
-                    <h5 class="card-title text-uppercase text-muted mb-0"> Collection Count</h5>
-                    <span class="h2 font-weight-bold mb-0">{{$data["collection_count"]}}</span>
-                </div>
-                <div class="col-auto">
-                    <div class="icon icon-shape bg-warning text-white rounded-circle shadow">
-                        <i class="fas fa-chart-pie"></i>
-                    </div>
-                </div>
-            </div>
-            <p class="mt-3 mb-0 text-muted text-sm">
-                <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 3.48%</span>
-                <span class="text-nowrap">Since last week</span>
-            </p>
-        </div>
-    </div>
-</div>
+
+
+
 
 
 <div class="col-xl-8 mb-5 mb-xl-0">
