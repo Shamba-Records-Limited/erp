5f4ab48e19ee9f762f89372cc3961e317da28276
Priscilla-22 <priscillamw123@gmail.com>
2024-10-21 12:44:26 +0300

    chore: add text for filter btn

--- resources/views/pages/common/wallet-management/account-payables/index.blade.php
+++ resources/views/pages/common/wallet-management/account-payables/index.blade.php
@@ -20,20 +20,28 @@ $acc_type = 'miller-admin';
             <div class="d-flex">
                 <button class="btn btn-primary" title="Add Filter" onclick="toggleFilterContainer()">
                     <span class="mdi mdi-filter"></span>
-                    <span class="mdi mdi-plus"></span>
+                    <span class="mdi mdi-plus">Add Filter</span>
                 </button>
                 <div class="dropdown ml-2">
-                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Make Payment
                     </button>
                     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" id="makePayment" href="{{route($acc_type.'.wallet-management.view-make-payment')}}">Make Payment</a>
+                        <a class="dropdown-item" id="makePayment"
+                            href="{{route($acc_type.'.wallet-management.view-make-payment')}}">Make Payment</a>
                     </div>
                 </div>
-                <a class="btn btn-primary ml-2" id="addOperationalExpense" href="{{route($acc_type.'.wallet-management.operational-expenses.add')}}">Add Operational Expense</a>
-                <button class="btn btn-primary btn-fw btn-sm ml-1" onclick="exportReport('{{route($exportRoute, 'xlsx')}}')"><span class="mdi mdi-file-excel"></span>Export Excel
+                <a class="btn btn-primary ml-2" id="addOperationalExpense"
+                    href="{{route($acc_type.'.wallet-management.operational-expenses.add')}}">Add Operational
+                    Expense</a>
+                <button class="btn btn-primary btn-fw btn-sm ml-1"
+                    onclick="exportReport('{{route($exportRoute, 'xlsx')}}')"><span
+                        class="mdi mdi-file-excel"></span>Export Excel
                 </button>
-                <button class="btn btn-primary btn-fw btn-sm ml-1" onclick="exportReport('{{route($exportRoute, 'pdf')}}')"><span class="mdi mdi-file-pdf"></span>Export Pdf
+                <button class="btn btn-primary btn-fw btn-sm ml-1"
+                    onclick="exportReport('{{route($exportRoute, 'pdf')}}')"><span
+                        class="mdi mdi-file-pdf"></span>Export Pdf
                 </button>
             </div>
 
@@ -42,19 +50,24 @@ $acc_type = 'miller-admin';
         </div>
 
         <div id="filter-display" class="d-flex filter-display align-items-start flex-wrap p-2">
-            <input hx-get="{{route($acc_type.'.wallet-management.account-payables.table')}}" hx-trigger="change" hx-target="#tableContent" hx-include=".table-control" hx-swap="innerHTML" name="filter" type="hidden" class="table-control" id="filter" value="" />
+            <input hx-get="{{route($acc_type.'.wallet-management.account-payables.table')}}" hx-trigger="change"
+                hx-target="#tableContent" hx-include=".table-control" hx-swap="innerHTML" name="filter" type="hidden"
+                class="table-control" id="filter" value="" />
             <div id="filter-container" class="border border-success rounded p-2 hidden">
                 <select class="form-control" id="filter-select" onchange="filterSelectChanged(myFilterOptions)">
                     <option value="">-- Select Filter --</option>
                 </select>
-                <select class="form-control ml-2" id="filter-operator" onchange="filterOperatorChanged(myFilterOptions)" disabled>
+                <select class="form-control ml-2" id="filter-operator" onchange="filterOperatorChanged(myFilterOptions)"
+                    disabled>
                     <option value="">-- Select Operator --</option>
                     <option value="is:">is:</option>
                 </select>
                 <input type="text" class="form-control ml-2" id="filter-value" disabled>
-                <button class="btn btn-primary ml-2" id="filter-apply" disabled onclick="filterApplyClicked()">Apply</button>
+                <button class="btn btn-primary ml-2" id="filter-apply" disabled
+                    onclick="filterApplyClicked()">Apply</button>
                 <button class="btn btn-secondary ml-2" id="filter-clear" onclick="filterClearClicked()">Clear</button>
-                <button class="btn btn-secondary ml-2" id="filter-close" onclick="toggleFilterContainer()">&times;</button>
+                <button class="btn btn-secondary ml-2" id="filter-close"
+                    onclick="toggleFilterContainer()">&times;</button>
             </div>
         </div>
 
@@ -62,7 +75,9 @@ $acc_type = 'miller-admin';
             <div class="d-flex align-items-center">
                 <label class="pt-1">Show:</label>
                 <div class="ml-2">
-                    <select hx-get="{{route($acc_type.'.wallet-management.account-payables.table')}}" hx-trigger="change" hx-target="#tableContent" hx-include=".table-control:not(#page)" hx-swap="innerHTML" class="form-control table-control" id="show-per-page" name="limit">
+                    <select hx-get="{{route($acc_type.'.wallet-management.account-payables.table')}}"
+                        hx-trigger="change" hx-target="#tableContent" hx-include=".table-control:not(#page)"
+                        hx-swap="innerHTML" class="form-control table-control" id="show-per-page" name="limit">
                         <option value="1">1</option>
                         <option value="5" selected>5</option>
                         <option value="10">10</option>
@@ -76,12 +91,16 @@ $acc_type = 'miller-admin';
             <div class="d-flex align-items-center">
                 <label for="search" class="mr-2">Search: </label>
                 <div>
-                    <input hx-get="{{route($acc_type.'.wallet-management.account-payables.table')}}" hx-trigger="keyup changed delay:500ms" hx-target="#tableContent" hx-include=".table-control:not(#page)" hx-swap="innerHTML" name="search" type="search" class="form-control table-control mb-2" placeholder="Search" aria-label="Search">
+                    <input hx-get="{{route($acc_type.'.wallet-management.account-payables.table')}}"
+                        hx-trigger="keyup changed delay:500ms" hx-target="#tableContent"
+                        hx-include=".table-control:not(#page)" hx-swap="innerHTML" name="search" type="search"
+                        class="form-control table-control mb-2" placeholder="Search" aria-label="Search">
                 </div>
             </div>
         </div>
 
-        <div id="tableContent" hx-get="{{route($acc_type.'.wallet-management.account-payables.table')}}" hx-trigger="load" hx-swap="innerHTML">
+        <div id="tableContent" hx-get="{{route($acc_type.'.wallet-management.account-payables.table')}}"
+            hx-trigger="load" hx-swap="innerHTML">
             <div class="skeleton" style="height: 20px; width: 100%;"></div>
         </div>
 
@@ -93,75 +112,75 @@ $acc_type = 'miller-admin';
 
 @push('custom-scripts')
 <script>
-    let myFilterOptions = new Map([
-        ["transaction_number", {}],
-        ["subject", {}],
-        ["sender", {}],
-        ["recipient", {}],
-        ["amount", {
-            "isNumeric": true
-        }],
-        ["created_at", {
-            "isNumeric": true
-        }],
-    ])
+let myFilterOptions = new Map([
+    ["transaction_number", {}],
+    ["subject", {}],
+    ["sender", {}],
+    ["recipient", {}],
+    ["amount", {
+        "isNumeric": true
+    }],
+    ["created_at", {
+        "isNumeric": true
+    }],
+])
 
-    $(document).ready(function() {
-        // init filter select
-        filterInitOptions(myFilterOptions);
-    });
+$(document).ready(function() {
+    // init filter select
+    filterInitOptions(myFilterOptions);
+});
 
 
-    function printReceipt(transactionId) {
-        $.ajax({
-            url: `/transaction-receipts/${transactionId}/print`,
-            method: 'GET',
-            success: function(resp) {
-                // alert(resp);
-                printContent(resp);
-            },
-            error: function(errResp) {
-                alert(errResp);
-            }
-        })
-    }
+function printReceipt(transactionId) {
+    $.ajax({
+        url: `/transaction-receipts/${transactionId}/print`,
+        method: 'GET',
+        success: function(resp) {
+            // alert(resp);
+            printContent(resp);
+        },
+        error: function(errResp) {
+            alert(errResp);
+        }
+    })
+}
 
-    function paginate(currentPage, lastPage) {
-        let paginationElem = createPaginationElem(currentPage, lastPage, function(pageNum) {
-            // on page click
-            if (pageNum < 1 || pageNum > lastPage) {
-                return;
-            }
-
-            if (pageNum == currentPage) {
-                return;
-            }
-
-            document.getElementById("page").value = pageNum;
-            document.getElementById("page").dispatchEvent(new Event('change'));
-        });
-
-        document.getElementById("items-pagination").replaceWith(paginationElem);
-    }
-
-    function exportReport(url) {
-        let tableControlElems = document.getElementsByClassName("table-control");
-
-        let form = document.createElement("form");
-        form.action = url;
-        form.method = "GET";
-
-        for (let elem of tableControlElems) {
-            // duplicate elem
-            let inp = document.createElement("input");
-            inp.type = "hidden";
-            inp.name = elem.name;
-            inp.value = elem.value;
-            form.appendChild(inp);
+function paginate(currentPage, lastPage) {
+    let paginationElem = createPaginationElem(currentPage, lastPage, function(pageNum) {
+        // on page click
+        if (pageNum < 1 || pageNum > lastPage) {
+            return;
         }
 
-        document.body.appendChild(form);
-        form.submit();
+        if (pageNum == currentPage) {
+            return;
+        }
+
+        document.getElementById("page").value = pageNum;
+        document.getElementById("page").dispatchEvent(new Event('change'));
+    });
+
+    document.getElementById("items-pagination").replaceWith(paginationElem);
+}
+
+function exportReport(url) {
+    let tableControlElems = document.getElementsByClassName("table-control");
+
+    let form = document.createElement("form");
+    form.action = url;
+    form.method = "GET";
+
+    for (let elem of tableControlElems) {
+        // duplicate elem
+        let inp = document.createElement("input");
+        inp.type = "hidden";
+        inp.name = elem.name;
+        inp.value = elem.value;
+        form.appendChild(inp);
     }
+
+    document.body.appendChild(form);
+    form.submit();
+}
 </script>
 @endpush
