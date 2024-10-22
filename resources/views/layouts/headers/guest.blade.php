113af2ff3cd4c7b953bb556cb32a6c0676b43f42
Priscilla-22 <priscillamw123@gmail.com>
2024-10-22 19:43:59 +0300

    style:reduce padding

--- resources/views/layouts/headers/guest.blade.php
+++ resources/views/layouts/headers/guest.blade.php
@@ -1,6 +1,6 @@
-<div class="header bg-gradient-custom-green py-7 py-lg-8">
+<div class="header bg-gradient-custom-green py-5 py-lg-8">
     <div class="container">
-        <div class="header-body text-center mb-7">
+        <div class="header-body text-center mb-3">
             <div class="row justify-content-center">
                 <div class="col-lg-5 col-md-6">
                     <h1 class="text-white">{{ __('Welcome to Shamba Traceability ERP.') }}</h1>
