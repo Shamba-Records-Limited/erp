ed9e7fe6ebe04eafe695d54a29b149718462d007
samuelcromwell <cromwellsamuel3@gmail.com>
2024-10-22 11:57:18 +0300

    Minor fixes

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -3,7 +3,8 @@
         <div class="copyright text-center text-xl-left text-muted">
             &copy; {{ now()->year }} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1"
                 target="_blank">Copyright ©</a> &amp;
-            <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">Shamba Traceability>
+            <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">Shamba Traceability
+            </a>
         </div>
     </div>
     <!-- <div class="col-xl-6">
