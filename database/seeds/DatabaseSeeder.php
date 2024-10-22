e7b844512c193f1b61e50e31553dad7b0727bb55
samuelcromwell <cromwellsamuel3@gmail.com>
2024-10-22 22:30:38 +0300

    Bug fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UsersTableSeeder::class]);
+        $this->call([RolesNPermissionsSeeder::class]);
+//$this->call([CooperativeSeeder::class]);
+//$this->call([UserSeeder::class]);
+//$this->call([CountiesSeeder::class]);
+$this->call([SubCountiesSeeder::class]);
+
     }
 }
