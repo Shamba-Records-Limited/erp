45394a9088ca9ad638c66e68e4c112f986cc0cb8
samuelcromwell <cromwellsamuel3@gmail.com>
2024-10-23 00:15:51 +0300

    Fix card dimensions

--- resources/views/pages/miller-admin/market-auction/products.blade.php
+++ resources/views/pages/miller-admin/market-auction/products.blade.php
@@ -8,14 +8,14 @@
 <div class="container mt-4">
     <div class="row">
         @foreach($paginatedProducts as $product)
-        <div class="col-md-4 col-lg-3 mb-4">
-            <div class="card product-card">
+        <div class="col-md-4 col-lg-3 mb-4 d-flex">
+            <div class="card product-card flex-fill" style="height: 100%;">
                 <!-- Lazy loading implemented here -->
-                <img src="{{ $product['image'] }}" class="card-img-top" alt="{{ $product['name'] }}" loading="lazy">
-                <div class="card-body">
+                <img src="{{ $product['image'] }}" class="card-img-top" alt="{{ $product['name'] }}" loading="lazy" style="object-fit: cover; height: 200px;">
+                <div class="card-body d-flex flex-column justify-content-between">
                     <h5 class="card-title">{{ $product['name'] }}</h5>
                     <p class="card-text">KES {{ number_format($product['price'], 2) }}</p>
-                    <button class="btn btn-primary add-to-cart-btn">Add to Cart</button>
+                    <button class="btn btn-success add-to-cart-btn mt-auto" style="background-color: #28a745; border-color: #28a745;">Add to Cart</button>
                 </div>
             </div>
         </div>
@@ -41,8 +41,9 @@ $(document).ready(function() {
     $('.add-to-cart-btn').on('click', function() {
         // Change the button text to "✔ Added"
         $(this).text('✔ Added');
-        // Change the button class from 'btn-primary' to 'btn-success' to make it green
-        $(this).removeClass('btn-primary').addClass('btn-success');
+        // Change the button color to a different shade of green
+        $(this).css('background-color', '#218838');
+        $(this).css('border-color', '#218838');
         // Disable the button to prevent further clicks
         $(this).prop('disabled', true);
     });
