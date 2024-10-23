798ee3a23fbe05fa4fd4b83b9acc9387800fd01b
Priscilla-22 <priscillamw123@gmail.com>
2024-10-23 12:55:59 +0300

    feat(wallet-management): enhance deposit form display

--- resources/views/pages/common/wallet-management/deposits/add.blade.php
+++ resources/views/pages/common/wallet-management/deposits/add.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @push('plugin-styles')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 @endpush
 
 @section('content')
@@ -8,76 +9,118 @@
 if (empty($acc_type)) {
 $acc_type = 'miller-admin';
 }
+
+// Function to format currency with commas
+function formatCurrency($amount) {
+return number_format($amount, 2);
+}
 @endphp
-<div class="card">
+
+<div class="card shadow-lg">
     <div class="card-body">
-        <div class="card-title">Deposit Funds</div>
-        <form action="{{route($acc_type.'.wallet-management.deposit')}}" method="post" enctype="multipart/form-data">
+        <h5 class="card-title text-center">Deposit Funds</h5>
+        <form action="{{ route($acc_type.'.wallet-management.deposit') }}" method="post" enctype="multipart/form-data">
             @csrf
-            <div>
-                <div>Account</div>
-                <div>Account Number: <span class="text-info font-weight-bold">{{$account->acc_number}}</span></div>
-                <div>Current Balance: <span class="text-info font-weight-bold">{{$account->balance}}</span></div>
+            <div class="mb-4">
+                <h4>Account Information</h4>
+                <p>Account Number: <strong class="semi-bold" style="color: black;">{{ $account->acc_number }}</strong>
+                </p>
+                <p>Current Balance: <strong class="text-success semi-bold">KES
+                        {{ formatCurrency($account->balance) }}</strong>
+                </p>
             </div>
+
             <div class="form-group">
-                <label>Amount</label>
-                <input type="number" class="form-control" name="amount" id="amount" placeholder="Enter Amount" value="{{old('amount', '')}}">
+                <label for="amount">Amount <span class="text-danger">*</span></label>
+                <input type="number" class="form-control" name="amount" id="amount" placeholder="Enter Amount"
+                    value="{{ old('amount', '') }}">
                 @if($errors->has('amount'))
-                <span class="help-block text-danger">
-                    <span>{{ $errors->first('amount') }}</span>
-                </span>
-                @endif
-            </div>
-            <!-- source of funds -->
-            <div class="form-group">
-                <label>Source of Funds</label>
-                <input type="text" class="form-control" name="source_of_funds" id="source_of_funds" placeholder="Enter Source of Funds" value="{{old('source_of_funds', '')}}">
-                <p>e.g. Bank, ATM, Cheque, etc.</p>
-                @if($errors->has('source_of_funds'))
-                <span class="help-block text-danger">
-                    <span>{{ $errors->first('source_of_funds') }}</span>
-                </span>
-                @endif
-            </div>
-            <div class="form-group">
-                <label>Description</label>
-                <input type="text" class="form-control" name="description" id="description" placeholder="Enter Description" value="{{old('description', '')}}">
-                @if($errors->has('description'))
-                <span class="help-block text-danger">
-                    <span>{{ $errors->first('description') }}</span>
-                </span>
-                @endif
-            </div>
-            <div class="d-none" id="imagePreviewContainer">
-                <div class="imageHolder pl-2">
-                    <img id="picturePreview" src="#" alt="pic" height="150px" width="150px" />
-                </div>
-            </div>
-            <div class="form-group">
-                <label>Deposit Slip</label>
-                <input type="file" class="form-control" name="deposit_slip" id="deposit_slip">
-                @if($errors->has('deposit_slip'))
-                <span class="help-block text-danger">
-                    <span>{{ $errors->first('deposit_slip') }}</span>
-                </span>
+                <span class="text-danger">{{ $errors->first('amount') }}</span>
                 @endif
             </div>
 
-            <div class="d-flex justify-content-start">
-                <button type="submit" class="btn btn-primary">Deposit</button>
-                <a href="{{route('miller-admin.wallet-management.view-deposit')}}" class="btn btn-primary ml-2">Cancel</a>
+            <div class="form-group">
+                <label for="source_of_funds">Source of Funds <span class="text-danger">*</span></label>
+                <input type="text" class="form-control" name="source_of_funds" id="source_of_funds"
+                    placeholder="Enter Source of Funds" value="{{ old('source_of_funds', '') }}">
+                <small class="form-text text-muted">e.g. Bank, ATM, Cheque, etc.</small>
+                @if($errors->has('source_of_funds'))
+                <span class="text-danger">{{ $errors->first('source_of_funds') }}</span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label for="description">Description</label>
+                <input type="text" class="form-control" name="description" id="description"
+                    placeholder="Enter Description" value="{{ old('description', '') }}">
+                @if($errors->has('description'))
+                <span class="text-danger">{{ $errors->first('description') }}</span>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label for="deposit_slip">Deposit Slip</label>
+                <input type="file" class="form-control" name="deposit_slip" id="deposit_slip">
+                @if($errors->has('deposit_slip'))
+                <span class="text-danger">{{ $errors->first('deposit_slip') }}</span>
+                @endif
+            </div>
+
+            <div class="d-none" id="imagePreviewContainer">
+                <div class="imageHolder">
+                    <img id="picturePreview" src="#" alt="Deposit Slip Preview" class="img-fluid rounded" />
+                </div>
+            </div>
+
+            <div class="d-flex justify-content-between mt-4">
+                <button type="submit" class="btn btn-success"><i class="fas fa-plus-circle"></i> Deposit</button>
+                <a href="{{ route('miller-admin.wallet-management.view-deposit') }}"
+                    class="btn btn-secondary">Cancel</a>
             </div>
         </form>
     </div>
 </div>
 @endsection
+
 @push('plugin-scripts')
 @endpush
 
 @push('custom-scripts')
 <script>
-    $('#deposit_slip').change(function() {
-        previewImage(this, 'picturePreview', 'imagePreviewContainer');
-    });
+$('#deposit_slip').change(function() {
+    previewImage(this, 'picturePreview', 'imagePreviewContainer');
+});
+
+function previewImage(input, imgElementId, containerId) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#' + imgElementId).attr('src', e.target.result);
+            $('#' + containerId).removeClass('d-none').addClass('d-block');
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
 </script>
 @endpush
+
+
+<!-- <style>
+.imageHolder {
+    text-align: center;
+    margin-top: 15px;
+}
+
+.img-fluid {
+    max-width: 100%;
+    height: auto;
+}
+
+.help-block {
+    margin-top: 5px;
+}
+
+.text-danger {
+    font-size: 0.875em;
+}
+</style> -->
