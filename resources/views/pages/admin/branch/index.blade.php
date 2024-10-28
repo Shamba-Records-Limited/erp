fa4d77223ca91a00588662ef3dfc70cda817bf9d
Priscilla-22 <priscillamw123@gmail.com>
2024-10-28 10:49:50 +0300

    "Commented out {{ $errors }} in admin branch index view"

--- resources/views/pages/admin/branch/index.blade.php
+++ resources/views/pages/admin/branch/index.blade.php
@@ -23,7 +23,7 @@
 
                     <form action="{{ route('branches.add') }}" method="post">
                         @csrf
-                        {{ $errors }}
+                        <!-- {{ $errors }} -->
                         <div class="form-row">
                             <div class="form-group col-lg-3 col-md-6 col-12">
                                 <label for="cooperative_id">Cooperative</label>
