85a4609a7d7fd580561337d5ce492265ad11c744
Priscilla-22 <priscillamw123@gmail.com>
2024-10-28 10:55:30 +0300

    "Fixed whitespace and formatting issues in cooperatives.blade.php"

--- resources/views/pages/admin/cooperatives.blade.php
+++ resources/views/pages/admin/cooperatives.blade.php
@@ -33,14 +33,15 @@ $countries = get_countries();
                             <div class="form-group col-12">
                                 <h5 class="mb-3">Cooperative Details</h5>
                             </div>
-                            <div class=" form-group col-lg-3 col-md-6 col-12">
+
+                            <div class="form-group col-lg-3 col-md-6 col-12">
                                 <label for="CompanyName">Name</label>
                                 <input type="text" name="cooperative_name"
                                     class="form-control {{ $errors->has('cooperative_name') ? ' is-invalid' : '' }}"
                                     id="CompanyName" placeholder="ABC" value="{{ old('cooperative_name')}}" required>
-
-                                @if ($errors->has(' cooperative_name')) <span class="help-block text-danger">
-                                    <strong>{{ $errors->first('cooperative_name')  }}</strong>
+                                @if ($errors->has('cooperative_name'))
+                                <span class="help-block text-danger">
+                                    <strong>{{ $errors->first('cooperative_name') }}</strong>
                                 </span>
                                 @endif
                             </div>
@@ -48,23 +49,23 @@ $countries = get_countries();
                             <div class="form-group col-lg-3 col-md-6 col-12">
                                 <label for="abbreviation">Abbreviation</label>
                                 <input type="text" name="abbr"
-                                    class="form-control  {{ $errors->has('abbr') ? ' is-invalid' : '' }}"
+                                    class="form-control {{ $errors->has('abbr') ? ' is-invalid' : '' }}"
                                     id="abbreviation" placeholder="A.B.C" value="{{ old('abbr')}}">
-
                                 @if ($errors->has('abbr'))
                                 <span class="help-block text-danger">
-                                    <strong>{{ $errors->first('abbr')  }}</strong>
+                                    <strong>{{ $errors->first('abbr') }}</strong>
                                 </span>
                                 @endif
                             </div>
+
                             <div class="form-group col-lg-3 col-md-6 col-12">
                                 <label for="location">Location</label>
                                 <input type="text" name="location"
-                                    class="form-control  {{ $errors->has('location') ? ' is-invalid' : '' }}"
+                                    class="form-control {{ $errors->has('location') ? ' is-invalid' : '' }}"
                                     value="{{ old('location')}}" id="location" placeholder="Nairobi" required>
                                 @if ($errors->has('location'))
                                 <span class="help-block text-danger">
-                                    <strong>{{ $errors->first('location')  }}</strong>
+                                    <strong>{{ $errors->first('location') }}</strong>
                                 </span>
                                 @endif
                             </div>
@@ -72,11 +73,11 @@ $countries = get_countries();
                             <div class="form-group col-lg-3 col-md-6 col-12">
                                 <label for="address">Address</label>
                                 <input type="text" name="address"
-                                    class="form-control  {{ $errors->has('address') ? ' is-invalid' : '' }}"
-                                    id="address" placeholder="Nairobi" value="{{ old('address')}}" required>
+                                    class="form-control {{ $errors->has('address') ? ' is-invalid' : '' }}" id="address"
+                                    placeholder="Nairobi" value="{{ old('address')}}" required>
                                 @if ($errors->has('address'))
                                 <span class="help-block text-danger">
-                                    <strong>{{ $errors->first('address')  }}</strong>
+                                    <strong>{{ $errors->first('address') }}</strong>
                                 </span>
                                 @endif
                             </div>
@@ -84,53 +85,50 @@ $countries = get_countries();
                             <div class="form-group col-lg-3 col-md-6 col-12">
                                 <label for="country_code">Country</label>
                                 <select name="country_code" id="country_code"
-                                    class=" form-control form-select {{ $errors->has('country_code') ? ' is-invalid' : '' }}"
-                                    value="Kenya">
+                                    class="form-control form-select {{ $errors->has('country_code') ? ' is-invalid' : '' }}">
                                     <option value=""> -Select Country-</option>
                                     @foreach($countries as $country)
                                     <option value="{{$country['code']}}" @if($country['name']=='Kenya' ) selected
                                         @endif>
-                                        {{ $country['name'] }}</option>
+                                        {{ $country['name'] }}
+                                    </option>
                                     @endforeach
-
-                                    @if ($errors->has('country_code'))
-                                    <span class="help-block text-danger">
-                                        <strong>{{ $errors->first('country_code')  }}</strong>
-                                    </span>
-                                    @endif
                                 </select>
+                                @if ($errors->has('country_code'))
+                                <span class="help-block text-danger">
+                                    <strong>{{ $errors->first('country_code') }}</strong>
+                                </span>
+                                @endif
                             </div>
 
                             <div class="form-group col-lg-3 col-md-6 col-12">
                                 <label for="county_id">Select County</label>
                                 <select name="county_id" id="county_id"
-                                    class=" form-control form-select {{ $errors->has('county_id') ? ' is-invalid' : '' }}">
+                                    class="form-control form-select {{ $errors->has('county_id') ? ' is-invalid' : '' }}">
                                     <option value=""> -Select County-</option>
                                     @foreach($counties as $county)
                                     <option value="{{$county->id}}"> {{ $county->name }}</option>
                                     @endforeach
-
-                                    @if ($errors->has('county_id'))
-                                    <span class="help-block text-danger">
-                                        <strong>{{ $errors->first('county_id')  }}</strong>
-                                    </span>
-                                    @endif
                                 </select>
+                                @if ($errors->has('county_id'))
+                                <span class="help-block text-danger">
+                                    <strong>{{ $errors->first('county_id') }}</strong>
+                                </span>
+                                @endif
                             </div>
 
                             <div class="form-group col-lg-3 col-md-6 col-12">
                                 <label for="sub_county">Select Sub County</label>
                                 <select data-subcounties="{{$sub_counties}}" name="sub_county_id" id="sub_county_id"
-                                    class=" form-control form-select {{ $errors->has('sub_county_id') ? ' is-invalid' : '' }}">
+                                    class="form-control form-select {{ $errors->has('sub_county_id') ? ' is-invalid' : '' }}">
                                     <option value=""> -Select Sub County-</option>
-
-                                    @if ($errors->has('sub_county_id'))
-                                    <span class="help-block text-danger">
-                                        <strong>{{ $errors->first('sub_county_id') }}</strong>
-                                    </span>
-                                    @endif
+                                </select>
+                                @if ($errors->has('sub_county_id'))
+                                <span class="help-block text-danger">
+                                    <strong>{{ $errors->first('sub_county_id') }}</strong>
+                                </span>
+                                @endif
                             </div>
-
 
                             <div class="form-group col-lg-3 col-md-6 col-12">
                                 <label for="company_email">Cooperative Email</label>
@@ -138,11 +136,9 @@ $countries = get_countries();
                                     class="form-control {{ $errors->has('cooperative_email') ? ' is-invalid' : '' }}"
                                     id="company_email" placeholder="info@abc.com" value="{{ old('cooperative_email')}}"
                                     required>
-
-
                                 @if ($errors->has('cooperative_email'))
                                 <span class="help-block text-danger">
-                                    <strong>{{ $errors->first('cooperative_email')  }}</strong>
+                                    <strong>{{ $errors->first('cooperative_email') }}</strong>
                                 </span>
                                 @endif
                             </div>
@@ -150,17 +146,15 @@ $countries = get_countries();
                             <div class="form-group col-lg-3 col-md-6 col-12">
                                 <label for="company_contact">Cooperative Contacts</label>
                                 <input type="text" name="cooperative_contact"
-                                    class="form-control {{ $errors->has('address') ? ' is-invalid' : '' }}"
+                                    class="form-control {{ $errors->has('cooperative_contact') ? ' is-invalid' : '' }}"
                                     id="company_contact" placeholder="07....." value="{{ old('cooperative_contact')}}"
                                     required>
-
                                 @if ($errors->has('cooperative_contact'))
                                 <span class="help-block text-danger">
-                                    <strong>{{ $errors->first('cooperative_contact')  }}</strong>
+                                    <strong>{{ $errors->first('cooperative_contact') }}</strong>
                                 </span>
                                 @endif
                             </div>
-
 
                             <div class="form-group col-lg-3 col-md-6 col-12">
                                 <label for="company_currency">Currency</label>
@@ -168,33 +162,28 @@ $countries = get_countries();
                                     class="form-control {{ $errors->has('cooperative_currency') ? ' is-invalid' : '' }}"
                                     id="company_currency" placeholder="Ksh." value="{{ old('cooperative_currency')}}"
                                     required>
-
                                 @if ($errors->has('cooperative_currency'))
                                 <span class="help-block text-danger">
-                                    <strong>{{ $errors->first('cooperative_currency')  }}</strong>
+                                    <strong>{{ $errors->first('cooperative_currency') }}</strong>
                                 </span>
                                 @endif
                             </div>
 
-
                             <div class="form-group col-lg-3 col-md-6 col-12">
-
-                                <label for="mainImage">Logo</label>
+                                <label for="company_logo">Logo</label>
                                 <div class="input-group">
                                     <div class="custom-file">
                                         <input type="file"
                                             class="custom-file-input @error('company_logo') is-invalid @enderror"
-                                            id="company_logo" name="company_logo" value="{{ old('company_logo') }}">
-                                        <label class="custom-file-label" for="exampleInputFile">Logo</label>
-
+                                            id="company_logo" name="company_logo">
+                                        <label class="custom-file-label" for="company_logo">Choose Logo</label>
                                         @if ($errors->has('company_logo'))
                                         <span class="help-block text-danger">
-                                            <strong>{{ $errors->first('company_logo')  }}</strong>
+                                            <strong>{{ $errors->first('company_logo') }}</strong>
                                         </span>
                                         @endif
                                     </div>
                                 </div>
-
                             </div>
 
                             <div class="form-group col-lg-3 col-md-6 col-12">
@@ -205,16 +194,18 @@ $countries = get_countries();
                                     <option value="">-- Select Main Product --</option>
                                     @foreach($products as $product)
                                     <option value="{{$product->id}}" @if($product->id == old('main_product_id'))
-                                        selected @endif>{{$product->name}}</option>
+                                        selected @endif>
+                                        {{$product->name}}</option>
                                     @endforeach
                                 </select>
                                 @if ($errors->has('main_product_id'))
                                 <span class="help-block text-danger">
-                                    <strong>{{ $errors->first('main_product_id')  }}</strong>
+                                    <strong>{{ $errors->first('main_product_id') }}</strong>
                                 </span>
                                 @endif
                             </div>
                         </div>
+
                         <hr class="mt-1 mb-1">
                         <h4 class=" mt-2 semi-bold">Contact Person</h4>
                         <div class="form-row">
