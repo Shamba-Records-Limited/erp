7c970b0dfbca054b5196aac774bb345586241d7f
Priscilla-22 <priscillamw123@gmail.com>
2024-11-07 22:09:52 +0300

    "Updated CoopBranchController to fetch collections excluding unit_price and added new columns to the query. Modified collections.blade.php to display new columns and calculate total quantity."

--- resources/views/pages/cooperative-admin/branches/collections.blade.php
+++ resources/views/pages/cooperative-admin/branches/collections.blade.php
@@ -14,23 +14,46 @@
                                 <th>Collection Number</th>
                                 <th>Lot Number</th>
                                 <th>Quantity</th>
-                                <th>Unit Price</th> <!-- Updated header to match `unit_price` -->
-                                <th>Branch Name</th>
+                                <th>Collection Time</th>
+                                <th>Product</th>
                             </tr>
                         </thead>
                         <tbody>
+                            @php
+                                $totalQuantity = 0; // Variable to hold the cumulative quantity
+                            @endphp
                             @foreach($collections as $key => $collection)
+                            @php
+                                $totalQuantity += $collection->quantity;
+                            @endphp
                             <tr>
                                 <td>{{ $key + 1 }}</td>
-                                <td>{{ \Carbon\Carbon::parse($collection->created_at)->format('Y-m-d') }}</td>
+                                <td>{{ \Carbon\Carbon::parse($collection->date_collected)->format('Y-m-d') }}</td>
                                 <td>{{ $collection->collection_number }}</td>
                                 <td>{{ $collection->lot_number }}</td>
-                                <td>{{ $collection->quantity }}</td>
-                                <td>{{ $collection->unit_price }}</td> <!-- Display `unit_price` -->
-                                <td>{{ $collection->branch_name }}</td>
+                                <td>{{ number_format($collection->quantity, 0) }}</td> <!-- Format quantity with commas -->
+                                <td>
+                                    @if($collection->collection_time == 1)
+                                        Morning
+                                    @elseif($collection->collection_time == 2)
+                                        Afternoon
+                                    @elseif($collection->collection_time == 3)
+                                        Evening
+                                    @else
+                                        Unknown
+                                    @endif
+                                </td>
+                                <td>{{ $collection->product_name }}</td>
                             </tr>
                             @endforeach
                         </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="4" class="text-right"><strong>Total Quantity:</strong></td>
+                                <td><strong>{{ number_format($totalQuantity, 0) }}</strong></td> <!-- Format total quantity with commas -->
+                                <td colspan="2"></td>
+                            </tr>
+                        </tfoot>
                     </table>
                 </div>
             </div>
