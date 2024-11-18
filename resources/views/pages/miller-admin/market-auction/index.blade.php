18f95f78f08a068ad3d965837487034ec993be88
samuelcromwell <cromwellsamuel3@gmail.com>
2024-11-18 16:48:44 +0300

    bg image

--- resources/views/pages/miller-admin/market-auction/index.blade.php
+++ resources/views/pages/miller-admin/market-auction/index.blade.php
@@ -5,10 +5,23 @@
 @endpush
 
 @section('content')
+<div class="row mt-6 mb-4 pl-6">
+    <div class="col-md-4 col-12 mb-4">
+        <div class="card shadow-lg border-0 rounded">
+            <div class="card-header text-center bg-gradient-primary text-white">
+                <h5 class="font-weight-bold mb-0 text-white">Total Number of Cooperatives/Aggregators</h5>
+            </div>
+            <div class="card-body text-center">
+                <h2 id="cooperative-count" class="font-weight-bold text-primary display-4">{{ count($cooperatives) }}</h2>
+                <!-- <p class="font-weight-bold text-muted">Cooperatives</p> -->
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="card pt-6">
     <div class="card-body">
         <div class="card-title">Market Auction</div>
-        <!-- <div class="card-subtitle">Cooperatives</div> -->
         <div class="table-responsive">
             <table class="table table-hover dt clickable">
                 <thead>
